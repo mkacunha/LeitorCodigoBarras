0f98611bcb67cad7bcecea378d76a3f3ef767da4
Maiko Cunha <mkacunha@gmail.com>
2016-05-19 17:13:22 -0300

    commit do projeto inteiro

---
+++ app/src/main/java/mkacunha/com/br/leitorcodigobarras/CaptureActivityAnyOrientation.java
@@ -0,0 +1,9 @@
+package mkacunha.com.br.leitorcodigobarras;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Created by maiko on 19/05/16.
+ */
+public class CaptureActivityAnyOrientation extends CaptureActivity {
+}
